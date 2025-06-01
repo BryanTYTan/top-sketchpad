cb48f843b7ea16b408a73e3a3075bfe9c3c648cb
bryan <90520318+BryanTYTan@users.noreply.github.com>
2025-06-01 16:02:31 +0800

    Made pad creationg dynamic, need to fix how box is displayed

--- src/js/sketchpad.js
+++ src/js/sketchpad.js
@@ -3,11 +3,13 @@ document.querySelector('.create-pad').addEventListener('click', initializePad);
 function initializePad(event) {
     var modal = document.querySelector('#popup_msg');
     var close_button = document.querySelector('.close-button');
-
+    var submit_button = document.querySelector('.generate-pad');
 
     close_button.addEventListener('click', function () {
         modal.style.display = "none";
     })
+
+    submit_button.addEventListener('click', generatePad);
 
     modal.style.display = "flex";
 
@@ -18,23 +20,36 @@ function initializePad(event) {
     }
 }
 
+function generatePad(event) {
+    var input = document.querySelector('#pad_size');
+    var value = input.value;
 
-var container_div = document.querySelector('.drawing-box');
+    if (value < 1 || value >=65) {
+        input.value = "";
+        alert('Please enter a valid number from 1 - 64');
+        return;
+    }
 
-// Create the drawing pad
-for (let i = 0; i < 16; i++) {
-    var new_div = document.createElement('div');
-    new_div.classList.add('drawing-pad');
-    container_div.appendChild(new_div);
+    var container_div = document.querySelector('.drawing-box');
+    container_div.innerHTML = "";
+
+    // Create the drawing pad
+    for (let i = 0; i < value; i++) {
+        var new_div = document.createElement('div');
+        new_div.classList.add('drawing-pad');
+        container_div.appendChild(new_div);
+    }
+
+    var boxes = document.querySelectorAll('.drawing-pad');
+
+    // Add the listener for each drawing pad to be "drawn" when hovered over
+    for (let i = 0; i < boxes.length; i++) {
+        boxes[i].addEventListener('mouseenter', mouseEnter);
+    }
+
+
+    document.querySelector('#popup_msg').style.display = "none";
 }
-
-var boxes = document.querySelectorAll('.drawing-pad');
-
-// Add the listener for each drawing pad to be "drawn" when hovered over
-for (let i = 0; i < boxes.length; i++) {
-    boxes[i].addEventListener('mouseenter', mouseEnter);
-}
-
 
 
 // Fuctions
